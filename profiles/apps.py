027971b50d73a4935aee3eb467de2a7afae2865a
nhieckqo <nhieckqo9@yahoo.com>
2021-05-01 21:25:59 +0800

    added profiles model \n - SIGNALS - trigger post_save event from one model to call functions like creating record to other related models

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+    # import signals.py
+    def ready(self):
+        import profiles.signals
