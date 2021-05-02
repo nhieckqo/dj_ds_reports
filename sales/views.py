8465ac8b1fcc2c33c3eefae6c4bf9308e04eb329
nhieckqo <nhieckqo9@yahoo.com>
2021-05-02 12:59:01 +0800

    added Search form

--- sales/views.py
+++ sales/views.py
@@ -2,12 +2,29 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 # Create your views here.
 
 
 def home_view(request):
+    form = SalesSearchForm(request.POST or None)
     hello = 'hello world form the view'
-    return render(request, 'sales/home.html', {'hello':hello})
+    context = {
+        'hello':hello,
+        'form': form,
+    }
+    return render(request, 'sales/home.html', context)
+
+# def sales_list_view(request):
+#     qs = Sale.objects.all()
+#     return render(request, 'sales/main.html', {'object_list':qs})
+
+# def sales_detail_view(request, **kwargs):
+#     pk = kwargs.get('pk')
+#     obj = Sale.objects.get(pk=pk)
+#     #or
+#     #obj = get_object_or_404(Sale, pk=pk)
+#     return render(request, 'sales/detail.html', {'object':obj})
 
 
 class SalesListView(ListView):
@@ -18,3 +35,4 @@ class SalesListView(ListView):
 class SalesDetailView(DetailView):
     model = Sale
     template_name = 'sales/detail.html'
+
