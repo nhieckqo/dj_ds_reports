41b6061b1a5364f841335ce8135b7ea895ddd00b
nhieckqo <nhieckqo9@yahoo.com>
2021-05-02 22:19:06 +0800

    added groupby on merged dataframes

--- sales/views.py
+++ sales/views.py
@@ -12,6 +12,7 @@ def home_view(request):
     sales_df = None
     positions_df = None
     merged_df = None
+    df = None
     form = SalesSearchForm(request.POST or None)
     
     if request.method == 'POST':
@@ -46,9 +47,12 @@ def home_view(request):
             # print(positions_df)
             merged_df = pd.merge(sales_df, positions_df, on='sales_id')
 
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df = merged_df.to_html()
+            df = df.to_html()
 
         else:
             print('no data')
@@ -69,6 +73,7 @@ def home_view(request):
         'sales_df':sales_df,
         'positions_df': positions_df,
         'merged_df': merged_df,
+        'df': df,
     }
     return render(request, 'sales/home.html', context)
 
