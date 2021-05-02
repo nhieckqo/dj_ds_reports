830b46c4b35044a7dc4e5031c4228fde7a1e040c
nhieckqo <nhieckqo9@yahoo.com>
2021-05-02 13:54:12 +0800

    PANDAS/DATAFRAME introduction

--- sales/views.py
+++ sales/views.py
@@ -3,14 +3,32 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 # Create your views here.
 
 
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    hello = 'hello world form the view'
+    
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+        print(date_from, date_to, chart_type)
+
+        qs = Sale.objects.filter(created__date=date_from)
+        obj = Sale.objects.get(id=1)
+        # print(qs)
+        # print(obj)
+        print('###########')
+        df1 = pd.DataFrame(qs.values())
+        print(df1)
+        # print('##########')
+        # df2 = pd.DataFrame(qs.values_list())
+        # print(df2)
+
     context = {
-        'hello':hello,
+        
         'form': form,
     }
     return render(request, 'sales/home.html', context)
