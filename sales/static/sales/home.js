5c9778d02e73cedde54d53a97739a1c5831fae32
nhieckqo <nhieckqo9@yahoo.com>
2021-05-14 08:16:05 +0800

    -added prepend chart img to modal

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -2,6 +2,7 @@ console.log('hello world!')
 
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
+const modalBdy = document.getElementById('modal-body')
 
 console.log(reportBtn)
 console.log(img)
@@ -10,3 +11,9 @@ console.log(img)
 if (img){
     reportBtn.classList.remove('not-visible')
 }
+
+reportBtn.addEventListener('click', ()=>{
+    console.log('clicked')
+    img.setAttribute('class', 'w-100')
+    modalBdy.prepend(img)
+})
